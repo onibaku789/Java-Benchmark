e2503f1900dcaca3660ed8a027696f5a461a0057
Attila Varga <varga.attila798@gmail.com>
2022-07-09 13:55:11 +0200

    Random data provider and objectmapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id ("me.champeau.jmh") version "0.6.6"
+    id("me.champeau.jmh") version "0.6.6"
 }
 
 group = "org.example"
@@ -14,8 +14,8 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-
-
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation("net.datafaker:datafaker:1.4.0")
 }
 
 tasks.getByName<Test>("test") {
